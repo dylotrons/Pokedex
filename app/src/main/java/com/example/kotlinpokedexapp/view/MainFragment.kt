46de0f51c06ad7cf7db880f63f4eb54d4411f22a
Danillo <dylotrons@gmail.com>
2022-03-26 16:27:58 -0300

    Added resources for creating components. Created Pokemon Item.

---
+++ app/src/main/java/com/example/kotlinpokedexapp/view/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinpokedexapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlinpokedexapp.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+}
